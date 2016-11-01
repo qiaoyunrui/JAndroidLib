b32d310254b6d3c28413d2f88577b39ff36aa8b8
Juhezi <juhezix@163.com>
2016-11-01 14:16:42 +0800

    test GlideLoader

--- sample/src/main/java/com/juhezi/jlib/MainActivity.java
+++ sample/src/main/java/com/juhezi/jlib/MainActivity.java
@@ -2,6 +2,7 @@ package com.juhezi.jlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.juhezi.jandroidlib.activities.JBaseActivity;
 import com.juhezi.jandroidlib.image.ABuilder;
@@ -11,22 +12,24 @@ import com.juhezi.jandroidlib.image.impl.glide.GlideBullider;
 
 public class MainActivity extends JBaseActivity {
 
+    private ImageView mImgShow1;
+
+    private String url = "http://tse1.mm.bing.net/th?id=OIP.Mc734fe5b13c5116748ce0505ae01681bo0&pid=15.1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImgShow1 = (ImageView) findViewById(R.id.img_show_1);
+
         BuilderFactory.createDefaultBulider()
-                .attachTo(null)
-                .loadFrom("Builder")
+                .attachTo(mImgShow1)
+                .loadFrom(url)
+                .with(this)
                 .build()
                 .load();
 
-        BuilderFactory.createBuilder(GlideBullider.class)
-                .attachTo(null)
-                .loadFrom("url")
-                .build()
-                .load();
     }
 
     @Override
